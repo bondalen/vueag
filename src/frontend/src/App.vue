ac3efd0153e4bcde5ebda9424e2a1710994fec42
bondalen <jjjalexbond@gmail.com>
2025-03-13 12:48:55 +0300

    frontend, bootstrap. Is work

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,11 +1,25 @@
 <template>
-  <v-app>
-    <v-main>
+  <div id="app">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <router-link to="/" class="navbar-brand">bezKoder</router-link>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link to="/tutorials" class="nav-link">Tutorials</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/add" class="nav-link">Add</router-link>
+        </li>
+      </div>
+    </nav>
+
+    <div class="container mt-3">
       <router-view />
-    </v-main>
-  </v-app>
+    </div>
+  </div>
 </template>
 
-<script setup>
-  //
+<script>
+export default {
+  name: "app"
+};
 </script>
--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,20 +1,8 @@
-/**
- * main.js
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
-// Plugins
-import { registerPlugins } from '@/plugins'
-
-// Components
-import App from './App.vue'
-
-// Composables
 import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
-const app = createApp(App)
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-registerPlugins(app)
-
-app.mount('#app')
+createApp(App).use(router).mount('#app')
