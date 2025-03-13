16e2165c9be011db4bc71ced100bc9484c017cba
bondalen <jjjalexbond@gmail.com>
2025-03-13 14:53:09 +0300

    v-data-table add. Before vuetify

---
+++ src/v-data-table/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- 3. И вот здесь, наконец, отрисовали -->
+  <DataTable />
+</template>
+
+<script>
+// 1. Вот здесь мы вытащили компонент из файловой системы
+import DataTable from './components/DataTable.vue';
+
+export default {
+  name: 'App',
+  components: {
+    // 2. Здесь мы добавили его в приложение vue
+    DataTable
+  }
+}
+</script>
---
+++ src/v-data-table/src/components/DataTable.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div>
+        <h4>Data Table</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="">
+    
+</style>
