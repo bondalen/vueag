4b0a0e26c4abf90a498a32dd6fc06eed3ca68d7e
bondalen <jjjalexbond@gmail.com>
2025-03-14 16:43:24 +0300

    VDataTable

--- src/v-data-table/src/components/DataTable.vue
+++ src/v-data-table/src/components/DataTable.vue
@@ -1,25 +1,49 @@
-<template lang="">
+<template>
+    <!-- если массив постов существуе, то отображаем этот раздел -->
     <div v-if="posts">
-        <h4>{{console.log(posts.value)}}</h4>
+        <v-data-table
+            v-model:items-per-page="itemsPerPage"
+                :headers="headers"
+                :items="data" item-value="userId"
+            class="elevation-1">
+        </v-data-table>
     </div>
-</template>
+  </template>
 
-<script>
-import getAllPosts from '@/services/getAllPosts';
+  <script>
+  // импортируем компонент таблицы данных из файловой системы
+  import { VDataTable } from 'vuetify/lib/components/index.mjs' //lib/components/VDataTable.mjs'
+  import getAllPosts from '../services/getAllPosts'
+  
+  export default {
+    components: {
+        // включаем компонент таблицы данных в файловую в наш компонент описываемый данным файлом
+      VDataTable,
+    },
 
-export default {
-
+    //как я понимаю, здесь функция срабатывающая при загрузке компонента
+    // которая устанавливает его состояние
     setup() {
-
-        const { post, error, load } = getAllPosts()
-        load()
-
-        return { post, error, load }
-    }
-    
-}
-</script>
-
-<style lang="">
-    
-</style>
+  
+      const { posts, error, load } = getAllPosts()
+      load()
+      console.log(posts.value);
+      return {
+        posts,
+        error,
+        itemsPerPage: 10,
+        // вот здесь определяем заголовки таблицы
+        headers: [
+          { title: 'User Id',   align: 'start',     sortable: false,    key: 'userId'},
+          { title: 'Title',     align: 'start',                         key: 'title' },
+          { title: 'Body',      align: 'start',                         key: 'body' }
+        ],
+        data: posts,
+      }
+  
+    },
+  }
+  </script>
+  <style lang="">
+  
+  </style>
